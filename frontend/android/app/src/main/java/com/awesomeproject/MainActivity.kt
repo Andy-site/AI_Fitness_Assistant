15b64e533ea206ead3dfd0f9075ebb8d7958859c
Andy-Site <aanandasaga02@gmail.com>
2025-01-11 23:35:49 +0545

    some minors

--- frontend/android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ frontend/android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -1,21 +1,19 @@
-package com.awesomeproject
+package com.awesomeproject // your actual package name
 
-import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.ReactActivityDelegateWrapper
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "AwesomeProject" // replace with your actual project name
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun getMainComponentName(): String {
-        return "AwesomeProject" // Replace with your app name
-    }
-
-    override fun createReactActivityDelegate(): ReactActivityDelegate {
-        return ReactActivityDelegate(this, mainComponentName)
-    }
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use DefaultReactActivityDelegate.
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName)
 }
