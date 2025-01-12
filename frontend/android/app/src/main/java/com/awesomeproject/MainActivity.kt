c99683412838615206d76fce6bedb9e6a2a2a788
Andy-Site <aanandasaga02@gmail.com>
2025-01-12 10:10:42 +0545

    Modified for screens

--- frontend/android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ frontend/android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.awesomeproject
 
-import android.os.Bundle;
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -17,10 +16,9 @@ class MainActivity : ReactActivity() {
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * which allows you to enable New Architecture with a single boolean flag [fabricEnabled]
    */
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
 
