acfcb12d1614e733ab2a7b5940ba5f838f659f93
Andy-Site <aanandasaga02@gmail.com>
2025-02-28 12:53:23 +0545

    c

--- frontend/android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ frontend/android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -1,45 +1,24 @@
 package com.awesomeproject // your actual package name
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
 import android.content.Intent
->>>>>>> Nutrition
 import android.os.Bundle
->>>>>>> 39bad6ba8f3b37b79b679f0e3837b0988915940d
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
     /**
      * Returns the name of the main component registered from JavaScript. This is used to schedule
      * rendering of the component.
      */
     override fun getMainComponentName(): String = "AwesomeProject" // replace with your actual project name
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     /**
      * Returns the instance of the [ReactActivityDelegate]. We use DefaultReactActivityDelegate.
      */
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName)
-}
-=======
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "AwesomeProject"
-=======
-    /**
-     * Returns the name of the main component registered from JavaScript. This is used to schedule
-     * rendering of the component.
-     */
-    override fun getMainComponentName(): String = "AwesomeProject"
->>>>>>> Nutrition
 
     /**
      * Ensures React Native receives the new intent when the app is opened from a notification
@@ -56,8 +35,4 @@ class MainActivity : ReactActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(null)
     }
-
-    override fun createReactActivityDelegate(): ReactActivityDelegate =
-        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
->>>>>>> 39bad6ba8f3b37b79b679f0e3837b0988915940d
