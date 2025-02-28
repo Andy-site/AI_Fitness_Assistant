467d1c0c083c60d287e14bf140f34c4a650f31d1
Andy-Site <aanandasaga02@gmail.com>
2025-02-28 12:40:03 +0545

    layout err

--- frontend/src/navigation/AppNavigator.jsx
+++ frontend/src/navigation/AppNavigator.jsx
@@ -2,10 +2,10 @@ import './gesture-handler';
 import React, { useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-<<<<<<< HEAD
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet } from 'react-native';
-=======
+
 import React from 'react';
 import EmailScreen from '../screens/Register/EmailScreen';
 import PasswordScreen from '../screens/Register/PasswordScreen';
@@ -26,9 +26,8 @@ import Home from '../screens/HomeAndLogin/HomeScreen';
 import ForgotPassword1 from '../screens/Login/ForgotPassword1';
 import ForgotPassword2 from '../screens/Login/ForgotPassword2';
 import ForgotPassword3 from '../screens/Login/ForgotPassword3';
->>>>>>> 39bad6ba8f3b37b79b679f0e3837b0988915940d
 
-<<<<<<< HEAD
+
 // Import Registration Screens
 import EmailScreen from '../screens/register/EmailScreen';
 import PasswordScreen from '../screens/register/PasswordScreen';
@@ -47,7 +46,7 @@ import Loading3 from '../screens/Loading/Loading3';
 import Loading4 from '../screens/Loading/Loading4';
 
 // Create Navigators
-=======
+
 import Workout from '../screens/HomeAndLogin/Workout';
 import Exercises from '../screens/HomeAndLogin/Exercises';
 import ExeDetails from '../screens/HomeAndLogin/ExeDetails';
@@ -70,7 +69,6 @@ import Notification from '../screens/Loading/Notification';
 
 
 
->>>>>>> b6897e0c166e38dcc801765204eb7b10bbe1f9f7
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -101,32 +99,16 @@ const AppNavigator = () => {
 
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <Stack.Navigator initialRouteName={isLoading ? 'Landing' : 'LoadingDrawer'} screenOptions={{ headerShown: false }}>
-        {/* Landing Screen */}
-        <Stack.Screen name="Landing" component={Landing} />
-
-        {/* Loading Screens */}
-        <Stack.Screen name="LoadingDrawer" component={LoadingDrawer} />
-
-        {/* Registration Flow */}
-        <Stack.Screen name="Email" component={EmailScreen} />
-=======
-      <Stack.Navigator initialRouteName="LoginScreen">
-=======
+      
 
 
         <Stack.Navigator initialRouteName="LoginScreen">
->>>>>>> b6897e0c166e38dcc801765204eb7b10bbe1f9f7
         <Stack.Screen name="Landing" component={Landing}  options={{ headerShown: false }}/>
         <Stack.Screen name="Loading1" component={Loading1}  options={{ headerShown: false }}/>
         <Stack.Screen name="Loading2" component={Loading2}  options={{ headerShown: false }}/>
         <Stack.Screen name="Loading3" component={Loading3} options ={{ headerShown: false }}/>
         <Stack.Screen name="Loading4" component={Loading4} options={{ headerShown: false }}/>
-<<<<<<< HEAD
         <Stack.Screen name="Email" component= { EmailScreen } />
->>>>>>> 39bad6ba8f3b37b79b679f0e3837b0988915940d
         <Stack.Screen name="PasswordScreen" component={PasswordScreen} />
         <Stack.Screen name="NameScreen" component={NameScreen} />
         <Stack.Screen name="AgeScreen" component={AgeScreen} />
@@ -138,7 +120,6 @@ const AppNavigator = () => {
         <Stack.Screen name="ForgotPassword1" component={ForgotPassword1} />
         <Stack.Screen name="ForgotPassword2" component={ForgotPassword2} />
         <Stack.Screen name="ForgotPassword3" component={ForgotPassword3} />
-=======
         <Stack.Screen name="Email" component= { EmailScreen } options={{ headerShown: false }} />
         <Stack.Screen name="PasswordScreen" component={PasswordScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="NameScreen" component={NameScreen} options={{ headerShown: false }}/>
@@ -151,7 +132,6 @@ const AppNavigator = () => {
         <Stack.Screen name="ForgotPassword1" component={ForgotPassword1} options={{ headerShown: false }}/>
         <Stack.Screen name="ForgotPassword2" component={ForgotPassword2} options={{ headerShown: false }}/>
         <Stack.Screen name="ForgotPassword3" component={ForgotPassword3} options={{ headerShown: false }}/>
->>>>>>> b6897e0c166e38dcc801765204eb7b10bbe1f9f7
 
 
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
@@ -179,7 +159,7 @@ const AppNavigator = () => {
 
 export default AppNavigator;
 
-// Styles
+
 const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
